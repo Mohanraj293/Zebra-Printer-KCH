2cb859fc015f82a190aba791242244e0effb0a20
Mohanraj293 <mohanrajrengarajr@gmail.com>
2025-07-29 01:45:26 +0530

    Tarka ui library added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.lazymohan.zebraprinter"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -57,4 +57,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.tarkaui)
+    implementation(libs.tarkaui.icons)
+    implementation(libs.fluent.system.icons)
+    implementation(libs.eam360ui)
 }
